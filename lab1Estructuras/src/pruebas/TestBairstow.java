7c6fb73028d1c11b08493ad8320e2d35219b52ef
144blue <aletru321@gmail.com>
2019-02-17 12:36:02 -0500

    pruebas

---
+++ lab1Estructuras/src/pruebas/TestBairstow.java
@@ -0,0 +1,27 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBairstow {
+
+	
+	@Test
+	public void BairstowMethodTest() {
+		
+	}
+	
+	@Test
+	public void Division_DerivadaTest() {
+		
+	}
+	
+	@Test
+	public void raicesTest() {
+		
+	}
+	
+	
+
+}
